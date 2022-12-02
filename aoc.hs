374258a08688dd8c94922b1660872cbbf382bda7
Logan M <>
2022-12-02 19:20:18 +1300

    Restructure file

--- aoc.hs
+++ aoc.hs
@@ -11,12 +11,17 @@ import Data.Coerce
 import Data.Ord
 import Data.Char
 
--- Day 1
--- main :: IO ()
--- main = readFile "day1input.txt" >>= print . getSum . maximum . map (foldMap $ Sum . read) . splitWhen null . lines
--- main = readFile "day1input.txt" >>= print . getSum . mconcat . take 3 . reverse . sort . map (foldMap $ Sum . read) . splitWhen null . lines
+main :: IO ()
+main = day2b
 
 -- Day 2
-main :: IO ()
--- main = readFile "day2input.txt" >>= print . sum . map ((\[a, b] -> b-87 + 3 * ((b-a-25)`mod`3)) . map (ord.head) . words) . lines
-main = readFile "day2input.txt" >>= print . sum . map ((\[a, b] -> 1 + (a+b-151)`mod`3 + (b-88) * 3) . map (ord.head) . words) . lines
+day2a :: IO ()
+day2a = readFile "day2input.txt" >>= print . sum . map ((\[a, b] -> b-87 + 3 * ((b-a-25)`mod`3)) . map (ord.head) . words) . lines
+day2b :: IO ()
+day2b = readFile "day2input.txt" >>= print . sum . map ((\[a, b] -> 1 + (a+b-151)`rem`3 + (b-88) * 3) . map (ord.head) . words) . lines
+
+-- Day 1
+day1a :: IO ()
+day1a = readFile "day1input.txt" >>= print . getSum . maximum . map (foldMap $ Sum . read) . splitWhen null . lines
+day1b :: IO ()
+day1b = readFile "day1input.txt" >>= print . getSum . mconcat . take 3 . reverse . sort . map (foldMap $ Sum . read) . splitWhen null . lines
